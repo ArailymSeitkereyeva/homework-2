278ac3d449354bcfbe508aeba5d75cba3df02061
Arai <seit.arailym@gmail.com>
2022-12-24 23:13:53 +0600

    weekdays and weekends

---
+++ 3rd_homework/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите цифру дня недели от 1 до 7");
+void WeekDay(int arg)
+{
+    if (arg == 6 || arg == 7) Console.WriteLine("Выходной день.");
+    else if (arg >= 1 && arg <= 5) Console.WriteLine("Будний день.");
+    else Console.WriteLine("Ошибка: от 1 до 7");
+}
+
+try
+{
+    int numWeek =Convert.ToInt32(Console.ReadLine());
+    WeekDay(numWeek);
+}
+catch
+{
+    Console.WriteLine("Ошибка выполнения");
+}
